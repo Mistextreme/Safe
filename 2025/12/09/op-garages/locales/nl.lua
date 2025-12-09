a8eed0e29972247b435e4073525cc7092e75621d
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:01 +0000

    Redmi 8 - op-garages/locales/nl.lua

---
+++ 2025/12/09/op-garages/locales/nl.lua
@@ -0,0 +1,37 @@
+Locales['nl'] = {
+    ['not_owner'] = "Je bent niet de eigenaar van dit voertuig",
+    ['checking_veh'] = "Zoeken naar voertuig....",
+    ['veh_impounded'] = "Voertuig met kenteken ~g~%s is in beslag genomen",
+    ['no_money'] = "Je hebt niet genoeg geld om het voertuig in beslag te nemen!",
+    ['cant_impound'] = "Het voertuig kan niet in beslag worden genomen.",
+    ['stop_veh'] = "Stop het voertuig om het in de garage te verbergen.",
+    ['garage_header_Vehicle'] = "Garage",
+    ['garage_header_Helicopter'] = "Helikopters",
+    ['garage_header_Plane'] = "Vliegtuigen",
+    ['garage_header_Boat'] = "Boten",
+
+    ['press_to_open_Vehicle'] = "Druk op 'E' om de garage te openen.",
+    ['press_to_hide_Vehicle'] = "Druk op 'E' om het voertuig te verbergen.",
+    ['press_to_open_Boat'] = "Druk op 'E' om de haven te openen.",
+    ['press_to_hide_Boat'] = "Druk op 'E' om de boot te verbergen.",
+    ['press_to_open_Plane'] = "Druk op 'E' om de vliegtuigloods te openen.",
+    ['press_to_hide_Plane'] = "Druk op 'E' om het vliegtuig te verbergen.",
+    ['press_to_open_Helicopter'] = "Druk op 'E' om het helikopterplatform te openen.",
+    ['press_to_hide_Helicopter'] = "Druk op 'E' om de helikopter te verbergen.",
+
+    ['garage_open_target_Vehicle'] = "Open Garage",
+    ['garage_hide_target_Vehicle'] = "Verberg Voertuig",
+    ['garage_open_target_Boat'] = "Open haven",
+    ['garage_hide_target_Boat'] = "Verberg boot",
+    ['garage_open_target_Plane'] = "Open Vliegtuighangar",
+    ['garage_hide_target_Plane'] = "Verberg vliegtuig",
+    ['garage_open_target_Helicopter'] = "Open Helipad",
+    ['garage_hide_target_Helicopter'] = "Verberg helikopter",
+
+    ['novehicles'] = "Je bezit geen voertuigen.",
+
+    ['showroom_leave'] = "Verlaat de showroom.",
+    ['showroom_leave_marker'] = "Druk op 'E' om de showroom te verlaten.",
+    ['repairEndSuccess'] = "Voertuig met kenteken: %s succesvol gerepareerd",
+    ["repairNoMoney"] = "Je hebt niet genoeg geld om het voertuig te repareren!"
+}
