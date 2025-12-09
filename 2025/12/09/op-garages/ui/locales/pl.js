f886d5cbb2452c7ea8d4d8d1863aac5bb7dd205e
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:14 +0000

    Redmi 8 - op-garages/ui/locales/pl.js

---
+++ 2025/12/09/op-garages/ui/locales/pl.js
@@ -0,0 +1,13 @@
+const translate = new Object();
+
+translate['garage-head-translation'] = "GARAŻ"
+translate['btn-impound'] = "Odholuj"
+translate['btn-take'] = "Wyciągnij"
+translate['btn-select'] = "Pokaż"
+translate['btn-repair'] = "Napraw"
+translate['impounding-vehicle-text'] = "Odholowywanie Pojazdu..."
+translate['repairing-vehicle-text'] = "Naprawianie pojazdu..."
+translate['repairing'] = "Naprawianie..."
+translate['impounding'] = "Odholowywanie..."
+translate['enter_showroom'] = "Wejdź do showroomu."
+translate['search-bar-text'] = "Szukaj tablicy rejestracyjnej lub nazwy pojazdu."
