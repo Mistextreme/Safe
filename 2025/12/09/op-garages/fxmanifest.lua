b04f1bdbf705a6e39a8eab33441b309ff24bd165
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:57:59 +0000

    Redmi 8 - op-garages/fxmanifest.lua

---
+++ 2025/12/09/op-garages/fxmanifest.lua
@@ -0,0 +1,45 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bagieta'
+version '1.0.6'
+description 'Advanced Garages by otherplanet.dev | ESX/QBCORE SUPPORTED.'
+lua54 'yes'
+
+ui_page 'ui/index.html'
+ 
+files {
+	'ui/**'
+}
+
+shared_scripts {
+	'framework/shared.lua',
+	'locales/*.lua',
+	'config/config.lua',
+	'config/*.lua'
+}
+
+client_scripts {
+	'framework/client/c_framework.lua',
+	'client/*.lua',
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'framework/server/s_framework.lua',
+	'server/*.lua',
+}
+
+escrow_ignore {
+	'framework/**',
+	'client/**',
+	'server/**',
+	'locales/**',
+	'config/*.lua'
+}
+dependency '/assetpacks'
