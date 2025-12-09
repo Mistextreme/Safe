3e33fea181e12b38a85c1adab074bfc2305b79d3
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:00 +0000

    Redmi 8 - op-garages/locales/es.lua

---
+++ 2025/12/09/op-garages/locales/es.lua
@@ -0,0 +1,37 @@
+Locales['es'] = {
+    ['not_owner'] = "No eres el dueño de este vehículo",
+    ['checking_veh'] = "Buscando vehículo....",
+    ['veh_impounded'] = "Vehículo con matrícula ~g~%s ha sido confiscado",
+    ['no_money'] = "¡No tienes suficiente dinero para confiscar el vehículo!",
+    ['cant_impound'] = "El vehículo no puede ser confiscado.",
+    ['stop_veh'] = "Detén el vehículo para esconderlo en el garaje.",
+    ['garage_header_Vehicle'] = "Garaje",
+    ['garage_header_Helicopter'] = "Helicópteros",
+    ['garage_header_Plane'] = "Aviones",
+    ['garage_header_Boat'] = "Barcos",
+
+    ['press_to_open_Vehicle'] = "Presiona 'E' para abrir el garaje.",
+    ['press_to_hide_Vehicle'] = "Presiona 'E' para esconder el vehículo.",
+    ['press_to_open_Boat'] = "Presiona 'E' para abrir el puerto.",
+    ['press_to_hide_Boat'] = "Presiona 'E' para esconder el barco.",
+    ['press_to_open_Plane'] = "Presiona 'E' para abrir el hangar de aviones.",
+    ['press_to_hide_Plane'] = "Presiona 'E' para esconder el avión.",
+    ['press_to_open_Helicopter'] = "Presiona 'E' para abrir el helipuerto.",
+    ['press_to_hide_Helicopter'] = "Presiona 'E' para esconder el helicóptero.",
+
+    ['garage_open_target_Vehicle'] = "Abrir garaje",
+    ['garage_hide_target_Vehicle'] = "Ocultar vehículo",
+    ['garage_open_target_Boat'] = "Abrir puerto",
+    ['garage_hide_target_Boat'] = "Ocultar barco",
+    ['garage_open_target_Plane'] = "Abrir hangar de aviones",
+    ['garage_hide_target_Plane'] = "Ocultar avión",
+    ['garage_open_target_Helicopter'] = "Abrir helipuerto",
+    ['garage_hide_target_Helicopter'] = "Ocultar helicóptero",
+
+    ['novehicles'] = "No tienes ningún vehículo.",
+
+    ['showroom_leave'] = "Salir del showroom.",
+    ['showroom_leave_marker'] = "Presiona 'E' para salir del showroom.",
+    ['repairEndSuccess'] = "Vehículo con matrícula: %s reparado con éxito",
+    ["repairNoMoney"] = "¡No tienes suficiente dinero para reparar el vehículo!"
+}
