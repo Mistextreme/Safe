9059de0064c5b78152bf0aaed92096e0b6780a14
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:04 +0000

    Redmi 8 - op-garages/locales/de.lua

---
+++ 2025/12/09/op-garages/locales/de.lua
@@ -0,0 +1,38 @@
+Locales['de'] = {
+    ['not_owner'] = "Du bist nicht der Besitzer dieses Fahrzeugs",
+    ['checking_veh'] = "Fahrzeug wird gesucht....",
+    ['veh_impounded'] = "Fahrzeug mit Kennzeichen ~g~%s wurde beschlagnahmt",
+    ['no_money'] = "Du hast nicht genug Geld, um das Fahrzeug zu beschlagnahmen!",
+    ['cant_impound'] = "Das Fahrzeug kann nicht beschlagnahmt werden.",
+    ['stop_veh'] = "Stoppe das Fahrzeug, um es in der Garage zu verstecken.",
+    ['garage_header_Vehicle'] = "Garage",
+    ['garage_header_Helicopter'] = "Hubschrauber",
+    ['garage_header_Plane'] = "Flugzeuge",
+    ['garage_header_Boat'] = "Boote",
+
+    ['press_to_open_Vehicle'] = "Drücke 'E', um die Garage zu öffnen.",
+    ['press_to_hide_Vehicle'] = "Drücke 'E', um das Fahrzeug zu verstecken.",
+    ['press_to_open_Boat'] = "Drücke 'E', um den Hafen zu öffnen.",
+    ['press_to_hide_Boat'] = "Drücke 'E', um das Boot zu verstecken.",
+    ['press_to_open_Plane'] = "Drücke 'E', um den Flugzeughangar zu öffnen.",
+    ['press_to_hide_Plane'] = "Drücke 'E', um das Flugzeug zu verstecken.",
+    ['press_to_open_Helicopter'] = "Drücke 'E', um den Helipad zu öffnen.",
+    ['press_to_hide_Helicopter'] = "Drücke 'E', um den Hubschrauber zu verstecken.",
+
+    ['garage_open_target_Vehicle'] = "Garage öffnen",
+    ['garage_hide_target_Vehicle'] = "Fahrzeug verstecken",
+    ['garage_open_target_Boat'] = "Hafen öffnen",
+    ['garage_hide_target_Boat'] = "Boot verstecken",
+    ['garage_open_target_Plane'] = "Flugzeughangar öffnen",
+    ['garage_hide_target_Plane'] = "Flugzeug verstecken",
+    ['garage_open_target_Helicopter'] = "Helipad öffnen",
+    ['garage_hide_target_Helicopter'] = "Hubschrauber verstecken",
+
+    ['novehicles'] = "Du besitzt keine Fahrzeuge.",
+
+    ['showroom_leave'] = "Verlassen Sie den Ausstellungsraum.",
+    ['showroom_leave_marker'] = "Drücken Sie 'E', um den Ausstellungsraum zu verlassen.",
+    ['repairEndSuccess'] = "Fahrzeug mit dem Kennzeichen: %s erfolgreich repariert",
+    ["repairNoMoney"] = "Du kannst es dir nicht leisten, das Fahrzeug zu reparieren!"
+
+}
