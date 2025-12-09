02b543e18dadc3943609b6c37108f455b8117998
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:24 +0000

    Redmi 8 - op-garages/config/vehicles.lua

---
+++ 2025/12/09/op-garages/config/vehicles.lua
@@ -0,0 +1,277 @@
+Config.VehiclesCategories = {
+    ["none"] = {
+        logo = ""
+    },
+    ["toyota"] = {
+        logo = "https://i0.wp.com/precisiondriversunlimited.com/wp-content/uploads/2018/03/toyota-logo-free-download-png.png?fit=1200%2C852&ssl=1&w=640"
+    },
+    ["bmw"] = {
+        logo = "https://cdn.freebiesupply.com/logos/large/2x/bmw-logo-png-transparent.png"
+    },
+    ["nissan"] = {
+        logo = "https://seeklogo.com/images/N/Nissan-logo-CCA990D6E0-seeklogo.com.png"
+    },
+    ["speedophile"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/speedophile.png"
+    },
+    ["porsche"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/porsche.png"
+    },
+    ["Dinka"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/dinka.png"
+    },
+    ["Grotti"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/grotti.png"
+    },
+    ["Weeny"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/weeny.png"
+    },
+    ["Lampadati"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/Lampadati.png"
+    },
+    ["Dewbauchee"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/Dewbauchee.png"
+    },
+    ["Übermacht"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/Ubermacht.png"
+    },
+    ["Vapid"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/vapid.png"
+    },
+    ["Imponte"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/imponte.png"
+    },
+    ["Declasse"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/declasse.png"
+    },
+    ["Bravado"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/bravado.png"
+    },
+    ["BF"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/bf.png"
+    },
+    ["Nagasaki"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/nagasaki.png"
+    },
+    ["Benefactor"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/benefactor.png"
+    },
+    ["Coil"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/coil.png"
+    },
+    ["Canis"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/canis.png"
+    },
+    ["Truffade"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/truffade.png"
+    },
+    ["Karin"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/karin.png"
+    },
+    ["Pfister"] = {
+        logo = "https://data.otherplanet.dev/scripts/assets/opgarages/pfister.png"
+    }     
+}
+
+Config.Vehicles = {
+    ["-726818442"] = {
+        label = "NISSAN GTR",
+        brand = "nissan",
+        image = "https://img.gta5-mods.com/q95/images/nissan-gtr-r35-add-on-tuning-extras-bodykits/75a58b-Screenshot_7.png"
+    },
+    ["1792287121"] = {
+        label = "BMW M4 GT3",
+        brand = "bmw",
+        image = "https://img.gta5-mods.com/q75/images/bmw-m4-gt3-22/524cb1-19.png"
+    },
+    ["-574236320"] = {
+        label = "TOYOTA SUPRA",
+        brand = "toyota",
+        image = "https://img.gta5-mods.com/q75/images/toyota-supra-gr-2020-add-on-tuning-liveries/994a35-GTA5_2021_02_21_20_48_03_509.jpg"
+    },
+    ["SEASHARK"] = {
+        label = "Seashark",
+        brand = "speedophile",
+        image = "https://data.otherplanet.dev/scripts/assets/opgarages/seashark.png"
+    },
+    ["MARQUIS"] = {
+        label = "Marquis",
+        brand = "speedophile",
+        image = "https://data.otherplanet.dev/scripts/assets/opgarages/marquis.png"
+    },
+    ["SVOLITO2"] = {
+        label = "Supervolito 2",
+        brand = "none",
+        image = "https://data.otherplanet.dev/scripts/assets/opgarages/svolito.png"
+    },
+    ["hycm4"] = {
+        label = "Hycade BMW M4",
+        brand = "bmw",
+        image = "https://cs3.gtaall.com/screenshots/4dc09/2023-04/original/f0db6d5f9b7b1924c397cbb43e7eaa3a56a2f3ee/1211065-GTA5-2023-04-07-16-48-22-343.jpg"
+    },
+    ["SHAMAL"] = {
+        label = "SHAMAL",
+        brand = "none",
+        image = "https://gtamag.com/images/photo/gta-mag-buckingham-shamal-gtao-504208.jpg"
+    },
+    ["m3g80"] = {
+        label = "BMW M3G80",
+        brand = "bmw",
+        image = "https://img.gta5-mods.com/q95/images/bmw-m3-g80-2022-add-on-tuning-lods-fivem-debadged-replace-eliominati/75d33a-10.png"
+    },
+    ["992gt3rs"] = {
+        label = "Porsche GT3RS",
+        brand = "porsche",
+        image = "https://cdn.discordapp.com/attachments/1250438375018725417/1250482574074052628/unknown.png?ex=66b8e2a6&is=66b79126&hm=b21fbc50535963e2776ee3d7469eca14f283245caf06bcfba8c01fc2d3abc1ea&"
+    },
+    ["BLISTA"] = {
+        label = "Blista",
+        brand = "Dinka",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/blista.png",
+    },
+    ["brioso"] = {
+        label = "Brioso",
+        brand = "Grotti",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/brioso.png",
+    },
+    ["issi2"] = {
+        label = "Issi",
+        brand = "Weeny",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/issi2.png",
+    },
+    ["felon"] = {
+        label = "Felon",
+        brand = "Lampadati",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/felon.png",
+    },
+    ["exemplar"] = {
+        label = "Exemplar",
+        brand = "Dewbauchee",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/exemplar.png",
+    },
+    ["sentinel"] = {
+        label = "Sentinel",
+        brand = "Übermacht",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/sentinel.png",
+    },
+    ["zion"] = {
+        label = "Zion",
+        brand = "Übermacht",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/zion.png",
+    },
+    ["blade"] = {
+        label = "Blade",
+        brand = "Vapid",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/blade.png",
+    },
+    ["chino"] = {
+        label = "Chino",
+        brand = "Vapid",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/chino.png",
+    },
+    ["chino2"] = {
+        label = "Chino Luxe",
+        brand = "Vapid",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/chino2.png",
+    },
+    ["nightshade"] = {
+        label = "Nightshade",
+        brand = "Imponte",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/nightshade.png",
+    },
+    ["sabregt2"] = {
+        label = "Sabre GT",
+        brand = "Declasse",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/sabre.png",
+    },
+    ["BURRITO"] = {
+        label = "Burrito",
+        brand = "Declasse",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/burrito3.png",
+    },
+    ["rumpo"] = {
+        label = "Rumpo",
+        brand = "Bravado",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/rumpo.png",
+    },
+    ["bison"] = {
+        label = "Bison",
+        brand = "Bravado",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/bison.png",
+    },
+    ["minivan"] = {
+        label = "Minivan",
+        brand = "Vapid",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/minivan.png",
+    },
+    ["bfinjection"] = {
+        label = "Bf Injection",
+        brand = "BF",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/bfinjection.png",
+    },
+    ["bifta"] = {
+        label = "Bifta",
+        brand = "BF",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/bifta.png",
+    },
+    ["blazer"] = {
+        label = "Blazer",
+        brand = "Nagasaki",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/blazer.png",
+    },
+    ["dubsta3"] = {
+        label = "Dubsta 6x6",
+        brand = "Benefactor",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/dubsta6x6.png",
+    },
+    ["brawler"] = {
+        label = "Brawler",
+        brand = "Coil",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/brawler.png",
+    },
+    ["kamacho"] = {
+        label = "Kamacho",
+        brand = "Canis",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/kamacho.png",
+    },
+    ["guardian"] = {
+        label = "Guardian",
+        brand = "Vapid",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/guardian.png",
+    },
+    ["adder"] = {
+        label = "Adder",
+        brand = "Truffade",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/adder.png",
+    },
+    ["rapidgt"] = {
+        label = "Rapid GT",
+        brand = "Dewbauchee",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/rapidgt.png",
+    },
+    ["sultan"] = {
+        label = "Sultan",
+        brand = "Karin",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/sultan.png",
+    },
+    ["neon"] = {
+        label = "Neon",
+        brand = "Pfister",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/neon.png",
+    },
+    ["tropos"] = {
+        label = "Tropos",
+        brand = "Lampadati",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/tropos.png",
+    },
+    ["bestiagts"] = {
+        label = "Bestia GTS",
+        brand = "Grotti",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/bestiagts.png",
+    },
+    ["gt500"] = {
+        label = "GT 500",
+        brand = "Grotti",
+        image = "https://data.otherplanet.dev/scripts/assets/vehicleshop/gt500.png",
+    },    
+}
