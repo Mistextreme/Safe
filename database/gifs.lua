3fcc7b247370a5298fa9bdd0b0511587901bb4f1
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 05:10:40 +0000

    Update gifs.lua - Added 1 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,7 @@
 -- FiveManage Gifs
 return {
+    ["grok_video_2025-12-03-11-05-33"] = {
+        label = "grok_video_2025-12-03-11-05-33",
+        image = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-11-05-33.gif?token=AO2YHHBS3L6K554UXHECIMTJG6XXE"
+    }
 }
