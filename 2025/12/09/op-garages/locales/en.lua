d00af3ac565ed9043d6c7adf42514738003a0582
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:05 +0000

    Redmi 8 - op-garages/locales/en.lua

---
+++ 2025/12/09/op-garages/locales/en.lua
@@ -0,0 +1,38 @@
+Locales['en'] = {
+    ['not_owner'] = "You are not the owner of this vehicle",
+    ['checking_veh'] = "Searching for vehicle....",
+    ['veh_impounded'] = "Vehicle with registration ~g~%s has been impounded",
+    ['no_money'] = "You cannot afford to impound the vehicle!",
+    ['cant_impound'] = "The vehicle cannot be impounded.",
+    ['stop_veh'] = "Stop the vehicle to hide it in the garage.",
+    ['garage_header_Vehicle'] = "Garage",
+    ['garage_header_Helicopter'] = "Helicopters",
+    ['garage_header_Plane'] = "Planes",
+    ['garage_header_Boat'] = "Boats",
+
+    ['press_to_open_Vehicle'] = "Press 'E' to open the garage.",
+    ['press_to_hide_Vehicle'] = "Press 'E' to hide the vehicle.",
+    ['press_to_open_Boat'] = "Press 'E' to open the harbor.",
+    ['press_to_hide_Boat'] = "Press 'E' to hide the boat.",
+    ['press_to_open_Plane'] = "Press 'E' to open the Airplane Hangar.",
+    ['press_to_hide_Plane'] = "Press 'E' to hide the plane.",
+    ['press_to_open_Helicopter'] = "Press 'E' to open the HeliPad.",
+    ['press_to_hide_Helicopter'] = "Press 'E' to hide the helicopter.",
+
+    ['garage_open_target_Vehicle'] = "Open Garage",
+    ['garage_hide_target_Vehicle'] = "Hide Vehicle",
+    ['garage_open_target_Boat'] = "Open harbor",
+    ['garage_hide_target_Boat'] = "Hide boat",
+    ['garage_open_target_Plane'] = "Open Airplane Hangar",
+    ['garage_hide_target_Plane'] = "Hide plane",
+    ['garage_open_target_Helicopter'] = "Open HeliPad.",
+    ['garage_hide_target_Helicopter'] = "Hide helicopter",
+
+    ['novehicles'] = "You don't own any vehicles.",
+
+    ['showroom_leave'] = "Leave showroom.",
+    ['showroom_leave_marker'] = "Press 'E' to leave showroom.",
+
+    ['repairEndSuccess'] = "Successfully repaired vehicle with plate: %s",
+    ["repairNoMoney"] = "You cannot afford to repair the vehicle!"
+}
