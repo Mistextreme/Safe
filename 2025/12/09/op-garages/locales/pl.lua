dfc6ed78bd894ea9bb66c83724865e07376ebb5c
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 04:58:02 +0000

    Redmi 8 - op-garages/locales/pl.lua

---
+++ 2025/12/09/op-garages/locales/pl.lua
@@ -0,0 +1,37 @@
+Locales['pl'] = {
+    ['not_owner'] = "Nie jesteś właścicielem tego pojazdu",
+    ['checking_veh'] = "Trwa poszukiwanie pojazdu....",
+    ['veh_impounded'] = "Odholowano pojazd o rejestracji ~g~%s",
+    ['no_money'] = "Nie stać cię na odholowanie pojazdu!",
+    ['cant_impound'] = "Nie można odholować pojazdu.",
+    ['stop_veh'] = "Zatrzymaj pojazd aby schować go do garażu.",
+    ['garage_header_Vehicle'] = "Garaż",
+    ['garage_header_Helicopter'] = "Helikoptery",
+    ['garage_header_Plane'] = "Samoloty",
+    ['garage_header_Boat'] = "Łodzie",
+
+    ['press_to_open_Vehicle'] = "Naciśnij 'E', aby otworzyć garaż.",
+    ['press_to_hide_Vehicle'] = "Naciśnij 'E', aby schować pojazd.",
+    ['press_to_open_Boat'] = "Naciśnij 'E', aby otworzyć port.",
+    ['press_to_hide_Boat'] = "Naciśnij 'E', aby schować łódź.",
+    ['press_to_open_Plane'] = "Naciśnij 'E', aby otworzyć hangar dla samolotów.",
+    ['press_to_hide_Plane'] = "Naciśnij 'E', aby schować samolot.",
+    ['press_to_open_Helicopter'] = "Naciśnij 'E', aby otworzyć lądowisko dla helikopterów.",
+    ['press_to_hide_Helicopter'] = "Naciśnij 'E', aby schować helikopter.",
+
+    ['garage_open_target_Vehicle'] = "Otwórz garaż",
+    ['garage_hide_target_Vehicle'] = "Schowaj pojazd",
+    ['garage_open_target_Boat'] = "Otwórz przystań",
+    ['garage_hide_target_Boat'] = "Schowaj łódź",
+    ['garage_open_target_Plane'] = "Otwórz hangar na samoloty",
+    ['garage_hide_target_Plane'] = "Schowaj samolot",
+    ['garage_open_target_Helicopter'] = "Otwórz lądowisko dla helikopterów",
+    ['garage_hide_target_Helicopter'] = "Schowaj helikopter",
+
+    ['novehicles'] = "Nie posiadasz zadnych pojazdow.",
+
+    ['showroom_leave'] = "Opuść showroom.",
+    ['showroom_leave_marker'] = "Naciśnij 'E', aby opuścić showroom.",
+    ['repairEndSuccess'] = "Pomyślnie naprawiono pojazd o tablicy rejestracyjnej: %s",
+    ["repairNoMoney"] = "Nie stać cię na naprawę pojazdu!"
+}
