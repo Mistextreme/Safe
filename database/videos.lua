d20beccd79a1902f0484a111e4d1b217306a87d5
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 05:27:02 +0000

    Update videos.lua - Added 14 new entries

--- database/videos.lua
+++ database/videos.lua
@@ -55,5 +55,25 @@ return {
     ["Drive_19"] = {
         label = "Drive_19",
         video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/Drive_19.mp4?token=AO2YHHHCOX7BS74KKKB43ITJG6ZNW"
+    },
+    ["37c14730-e4dd-4021-8fa6-abb83e48a365_new"] = {
+        label = "37c14730-e4dd-4021-8fa6-abb83e48a365_new",
+        video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/37c14730-e4dd-4021-8fa6-abb83e48a365.mp4?token=AO2YHHFRDNCQVBK3XVMVDBDJG6ZUC"
+    },
+    ["8bb1c2ccd16342a6fb05a993ab8bac7c_new"] = {
+        label = "8bb1c2ccd16342a6fb05a993ab8bac7c_new",
+        video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/8bb1c2ccd16342a6fb05a993ab8bac7c.mp4?token=AO2YHHCNTYM5DRGB5DU3JNTJG6ZUC"
+    },
+    ["grok_video_2025-12-03-10-50-44_new"] = {
+        label = "grok_video_2025-12-03-10-50-44_new",
+        video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-10-50-44.mp4?token=AO2YHHCV22TXCLSSAZUJU4TJG6ZUC"
+    },
+    ["grok_video_2025-12-03-10-54-23_new"] = {
+        label = "grok_video_2025-12-03-10-54-23_new",
+        video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-10-54-23.mp4?token=AO2YHHDPQNA6TTJ2HEDZY6LJG6ZUC"
+    },
+    ["grok_video_2025-12-03-11-05-33_new_1"] = {
+        label = "grok_video_2025-12-03-11-05-33_new_1",
+        video = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-11-05-33.mp4?token=AO2YHHC2QYO5XCPSYO7YNALJG6ZUC"
     }
 }
