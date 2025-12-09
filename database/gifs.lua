290adbe7ed482139a582daea02d073b9e23dfc4c
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-09 05:27:03 +0000

    Update gifs.lua - Added 1 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -3,5 +3,9 @@ return {
     ["grok_video_2025-12-03-11-05-33"] = {
         label = "grok_video_2025-12-03-11-05-33",
         image = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-11-05-33.gif?token=AO2YHHBS3L6K554UXHECIMTJG6XXE"
+    },
+    ["grok_video_2025-12-03-11-05-33_new"] = {
+        label = "grok_video_2025-12-03-11-05-33_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/Safe/main/2025/12/09/grok_video_2025-12-03-11-05-33.gif?token=AO2YHHFCX5WFQLZZHUSRAETJG6ZUC"
     }
 }
