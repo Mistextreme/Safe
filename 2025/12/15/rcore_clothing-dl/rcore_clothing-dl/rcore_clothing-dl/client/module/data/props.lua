f4917d93cf45bc23662d5c71cdb10d1c37daf426
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:35:08 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/module/data/props.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/module/data/props.lua
@@ -0,0 +1,19 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+local L0_1, L1_1
+L0_1 = Citizen
+L0_1 = L0_1.CreateThread
+function L1_1()
+  local L0_2, L1_2
+  L0_2 = PlayerPedId
+  L0_2 = L0_2()
+end
+L0_1(L1_1)
