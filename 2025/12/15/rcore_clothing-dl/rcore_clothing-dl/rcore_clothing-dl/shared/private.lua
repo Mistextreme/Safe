fe22f164f34c9d7698ceeff2863cd68889c13401
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:59:35 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/shared/private.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/shared/private.lua
@@ -0,0 +1,13 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+local L0_1, L1_1
+BACKEND_URL = "https://clothing.rcore.cz/api"
+META_BACKEND_URL = "https://clothing.rcore.cz/meta/metadata/generate"
