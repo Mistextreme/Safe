b8c799b2ec4cb28566cdff2d4b0a9a00f4179cec
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:34:01 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/blips.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/blips.lua
@@ -0,0 +1,32 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+local function CreateShopBlip(coords, blipTitle, blipColor, blipId, blipScale)
+	local blip = AddBlipForCoord(coords)
+    SetBlipSprite(blip, blipId)
+	SetBlipDisplay(blip, 4)
+	SetBlipScale(blip, blipScale)
+	SetBlipColour(blip, blipColor)
+	SetBlipAsShortRange(blip, true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(blipTitle)
+	EndTextCommandSetBlipName(blip)
+end
+Citizen.CreateThread(function()
+    for id, shop in pairs(Config.ClothingShops) do
+        if shop.blip and shop.blip.title then
+            local blipScale = shop.scale
+            if blipScale == nil then
+                blipScale = 0.9
+            end
+            CreateShopBlip(shop.pos, shop.blip.title, shop.blip.colour, shop.blip.id, blipScale)
+        end
+    end
+end)
