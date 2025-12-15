5f2555dbebb1a64dda38980fa36f1d2f1cd4c126
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 17:16:26 +0000

    Upload: vms_housing/integration/[dispatch]/rcore_dispatch/client.lua

---
+++ 2025/12/15/vms_housing/integration/[dispatch]/rcore_dispatch/client.lua
@@ -0,0 +1,36 @@
+if Config.Dispatch ~= 'rcore_dispatch' then
+    return
+end
+
+function DispatchAlertClient(property, type)
+    local coords = nil
+    if property.type == 'mlo' then
+        coords = vector3(property.metadata.menu.x, property.metadata.menu.y, property.metadata.menu.z)
+    else
+        coords = vector3(property.metadata.enter.x, property.metadata.enter.y, property.metadata.enter.z)
+    end
+
+    if not coords then
+        return
+    end
+
+    TriggerServerEvent('rcore_dispatch:server:sendAlert', {
+        code = '459',
+        default_priority = 'high',
+        coords = coords,
+        job = Config.Alarm.AlarmJobs,
+        text = TRANSLATE('notify.lockpick:alarm_' .. type),
+        type = 'alerts',
+        -- blip_time = 5,
+        -- image = 'url_to_image.jpg',
+        -- custom_sound = 'url_to_sound.mp3',
+        blip = {
+            sprite = 480,
+            colour = 1,
+            scale = 1.0,
+            text = 'Burglary',
+            flashes = false,
+            radius = 20.0,
+        }
+    })
+end
