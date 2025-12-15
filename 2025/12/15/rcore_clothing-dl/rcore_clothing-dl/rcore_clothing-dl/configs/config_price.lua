115a6eb7d30dd0437a5507080c8dab5f595dd87a
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:59:44 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/configs/config_price.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/configs/config_price.lua
@@ -0,0 +1,41 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+Config.GlobalPriceMultiplier = 1.0
+Config.ShopMultiplier = {
+    binco = 1.0,
+    suburban = 5.0,
+    ponsonbys = 15.0,
+    ammunation = 4.0,
+    beach_masks = 4.0,
+    vangelico = 30.0,
+    default = 1.0,
+}
+Config.EverythingEverywhereIsFree = false -- Make everything free in all shops
+Config.CharFeaturesPrices = {
+    [SHOP_STRUCTURE.CHAR_PED_SELECT] = 0,
+    [SHOP_STRUCTURE.CHAR_HEADBLEND] = 0,
+    [SHOP_STRUCTURE.CHAR_NONFREEMODE_HEAD] = 0,
+    [SHOP_STRUCTURE.CHAR_FACE_FEATURES] = 0,
+    [SHOP_STRUCTURE.CHAR_BLEMISH] = 0,
+    [SHOP_STRUCTURE.CHAR_AGE] = 0,
+    [SHOP_STRUCTURE.CHAR_COMPLEXION] = 0,
+    [SHOP_STRUCTURE.CHAR_SUN_TAN] = 0,
+    [SHOP_STRUCTURE.CHAR_MOLES] = 0,
+    [SHOP_STRUCTURE.CHAR_BODY_BLEMISH] = 0,
+    [SHOP_STRUCTURE.CHAR_EYE_COLOR] = 0,
+    [SHOP_STRUCTURE.HAIR_HAIR] = 0,
+    [SHOP_STRUCTURE.HAIR_BEARD] = 0,
+    [SHOP_STRUCTURE.HAIR_EYEBROWS] = 0,
+    [SHOP_STRUCTURE.HAIR_CHEST] = 0,
+    [SHOP_STRUCTURE.MAKEUP_MAKEUP] = 0,
+    [SHOP_STRUCTURE.MAKEUP_BLUSH] = 0,
+    [SHOP_STRUCTURE.MAKEUP_LIPSTICK] = 0,
+}
