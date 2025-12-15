2a4d05b2338a1da9418e5c60c7f2afab693c1b9e
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:25:26 +0000

    Upload: NoPEDOX/fxmanifest.lua

---
+++ 2025/12/15/NoPEDOX/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game {'gta5'}
+description 'PED Inventory'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts {'client.lua', 'ConfigPEDUI.lua'}
+
+escrow_ignore {
+    'ConfigPEDUI.lua',
+}
