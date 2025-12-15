a5d7de3c2470ddd69011b020cfb7c6a3bb2c0b99
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:35:22 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/framework/custom.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/client/framework/custom.lua
@@ -0,0 +1,17 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+CreateThread(function()
+    if Config.Framework == 3 then
+        ShowNotification = function(text)
+            print(text)
+        end
+    end
+end)
