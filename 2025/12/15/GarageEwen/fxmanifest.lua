17a1d0a3a103c8e78a86929ef1c468b81526a96e
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:25:37 +0000

    Upload: GarageEwen/fxmanifest.lua

---
+++ 2025/12/15/GarageEwen/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MadeInLeak'
+description '.gg/MadeInLeak by Sanoo'
+
+client_scripts {
+  'client/garage.lua',
+}
+
+server_scripts {
+  'server/garage.lua',
+}
+
+shared_scripts {
+  'shared/config.lua',
+}
