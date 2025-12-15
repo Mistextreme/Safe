4fde105285623ebf95593ed328088488ee404d5b
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 16:33:52 +0000

    Upload: rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/permissions.lua

---
+++ 2025/12/15/rcore_clothing-dl/rcore_clothing-dl/rcore_clothing-dl/permissions.lua
@@ -0,0 +1,26 @@
+-- ============================================
+-- More exclusive content you will find here:
+-- Cleaned and working - hot scripts and more.
+--
+-- https://unlocknow.net/releases
+-- https://discord.gg/unlocknoww
+-- ============================================
+
+
+
+PermissionMap = {
+    ['group.admin'] = {
+        Permissions.ADMIN_STOCK_MANAGEMENT,
+        Permissions.ADMIN_EDIT_METADATA,
+        Permissions.ADMIN_EDIT_ARMS,
+        Permissions.ADMIN_OUTFIT_EDITOR,
+        Permissions.USE_ADMIN_MODE,
+        Permissions.ADMIN_IMAGE_DEBUG,
+        "command." .. Config.Commands.Skin,
+        "command." .. Config.Commands.ReloadSkin,
+    },
+    ['builtin.everyone'] = {
+        Permissions.TOGGLE_ID_ONLY_MODE,
+        "command." .. Config.Commands.ClearPed,
+    },
+}
