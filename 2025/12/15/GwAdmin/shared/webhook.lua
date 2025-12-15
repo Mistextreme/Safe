cbe2be14a031b7b8d97fa76dc3285ff508212bd8
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-15 15:58:04 +0000

    Upload: GwAdmin/shared/webhook.lua

---
+++ 2025/12/15/GwAdmin/shared/webhook.lua
@@ -0,0 +1,20 @@
+webhookList = {
+    ["setjob"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["instance"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["giveMoney"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["giveItem"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["jail"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["kick"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["wipe"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["goto"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["bringBack"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["birth"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["spectate"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["warn"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["revive"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["heal"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["ban"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+    ["clothes_relod"] = "https://discordapp.com/api/webhooks/1320958342813843497/EgdYI6pxhk3SgweM8Z8VKpSoLoGwIwsuZgqEtAjueONx4DKVyDW5QobScGL8qCMWs-bo",
+}
+
+--print("^2[webhook]^0 Loaded webhook.lua")
